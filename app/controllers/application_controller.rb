d5964a449a4ab4a1684a0383382ff5a7f0f7c3a4
6akayas <sayakach6@gmail.com>
2017-02-22 10:50:18 +0800

    fixed password error, added user#edit page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    render 'application/index'
+    render 'application/application'
   end
 end
