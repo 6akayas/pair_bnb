a770cf89c720e1248b1bf92fabcccf0ad93071be
6akayas <sayakach6@gmail.com>
2017-02-23 16:54:10 +0800

    posting listings done

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,5 @@
 class Listing < ApplicationRecord
-  belongs_to :users
+    belongs_to :user
+    validates :price, :home_title, :home_type, :room_type, :accomodate, :bedroom, :bathroom, :summary, :address, presence: true
 
-
-  
 end
