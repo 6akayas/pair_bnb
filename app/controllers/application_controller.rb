fb2f76f2ab97eaec3334a63d32ff3e5671e73d8c
6akayas <sayakach6@gmail.com>
2017-04-05 18:24:30 +0800

    fixed navbar links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
-    include Clearance::Controller
-    protect_from_forgery with: :exception
-    add_flash_types :danger, :info, :warning, :success
+  include Clearance::Controller
+  protect_from_forgery with: :exception
+  add_flash_types :danger, :info, :warning, :success
 
-    def index
+  def index
+  	if params[:search].present?
+      @listings = Listing.perform_search(params[:search])
+    else
+      @listings = Listing.all
     end
+  end  
+
+  def show
+		@user = User.find_by(id: params[:id])
+  end 
 	
 		# # authorization code
  #    def allowed?
