d09d77fbac1834f7e7562a44b1b7e570097b7436
6akayas <sayakach6@gmail.com>
2017-04-05 16:09:45 +0800

    fixed searchbar layout. need to fix search controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -4,7 +4,7 @@ class ListingsController < ApplicationController
         if params[:search].present?
             @listings = Listing.perform_search(params[:search])
         else
-            @listings = Listing.paginate(:page => params[:page], :per_page => 3)
+            @listings = Listing.all
         end
     end
 
