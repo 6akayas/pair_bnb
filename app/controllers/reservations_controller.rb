e047383ade0bd1d725480e2bf24b519e0ba3c7b5
6akayas <sayakach6@gmail.com>
2017-05-05 14:08:13 +0800

    fixed reservation controllr

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,55 +1,51 @@
 class ReservationsController < ApplicationController
 
+  def preview
+    start_date = Date.parse(params[:start_date])
+    end_date = Date.parse(params[:end_date])
 
-    def preview
-        start_date = Date.parse(params[:start_date])
-        end_date = Date.parse(params[:end_date])
+    output = {
+        conflict: is_conflict(start_date, end_date)
+    }
 
-        output = {
-            conflict: is_conflict(start_date, end_date)
-        }
+    render json: output
+  end
 
-        render json: output
-    end
+  def index
+    @reservations = Reservation.find_by(user_id: params[:listing_id])
+  end
 
+  def new
+    @reservation = Reservation.new
+  end
 
-    def index
-        @reservations = Reservation.find_by(listing_id: params[:listing_id])
-    end
+  def create
+    @listing = Listing.find_by(id: params[:id])
+    @reservation = current_user.reservations.new(reservation_params)
+    @reservation.listing_id = params[:listing_id]
+    @reservation.total_price = @reservation.listing.price*(@reservation.end_date - @reservation.start_date)
+      if @reservation.save
+          redirect_to listing_reservation_path(@reservation.id), success: "Reservation saved!"
+      else
+          redirect_to new_listing_reservation_path(params[:listing_id]), danger: "Reservation not successful!"
+      end
+  end
 
-    def new
-        @reservation = Reservation.new
+  def show
+    @reservation = Reservation.find(params[:listing_id])
+  end
 
-    end
+  private
 
-    def create
-        @listing = Listing.find_by(id: params[:id])
-        @reservation = current_user.reservations.new(reservation_params)
-        @reservation.listing_id = params[:listing_id]
-        @reservation.total_price = @reservation.listing.price*(@reservation.end_date - @reservation.start_date)
-        if @reservation.save
-            redirect_to listing_reservation_path(@reservation.id), success: "Reservation saved!"
-        else
-            redirect_to new_listing_reservation_path(params[:listing_id]), danger: "Reservation not successful!"
-        end
-    end
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date)
+  end
 
-    def show
-        @reservation = Reservation.find(params[:listing_id])
-    end
+  def is_conflict(start_date, end_date)
+    @listing = Listing.find(params[:listing_id])
 
-
-    private
-
-    def reservation_params
-        params.require(:reservation).permit(:start_date, :end_date)
-    end
-
-    def is_conflict(start_date, end_date)
-        @listing = Listing.find(params[:listing_id])
-
-        check = @listing.reservations.where("? < start_date AND end_date < ?", start_date, end_date)
-        check.size > 0? true: false
-    end
+  check = @listing.reservations.where("? < start_date AND end_date < ?", start_date, end_date)
+    check.size > 0? true: false
+  end
 
 end
